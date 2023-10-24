08b8af950983512865d13a3f825f5df2181f94af
ravikumar_repo <54144086+ravikumarcloudcka@users.noreply.github.com>
2023-10-24 10:36:35 +0530

    Apptier

---
+++ DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1.cluster-cywps23pzrex.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Devopslab3',
+    DB_DATABASE : 'webapp'
+});
